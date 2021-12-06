002b8e535af190557d3c4bb1fb0967e26d2128aa
Kochevnikbbb <bbelekov@mail.ru>
2021-12-07 01:16:12 +0600

    вппвп

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,44 @@
 package com.company;
 
 public class Main {
+    //public static Gods[] gods = new Gods[2];
 
     public static void main(String[] args) {
 	// write your code here
-        createObject("Ares").print();
-        createObject("Afina").print();
-        createObject("Zevs").print();
+        //gods[0]=new Ares();
+
+        //createObject("Ares").print();
+        //createObject("Afina").print();
+        //createObject("Zevs").print();
+
+
+        Gods[] gods = {createObject("Ares"),createObject("Afina"),createObject("Zevs")};
+        /*for (int i = 0; i < gods.length; i++) {
+            gods[i].print();
+        }*/
+        for (Gods a:gods) {
+            a.print();
+        }
+
+
+
+
 
 
     }
-    public static Printable createObject(String Gods){
-        Printable printable = null;
+    public static Gods createObject(String Gods){
+        //Printable printable = null;
         switch (Gods){
             case "Ares":
-                printable = new Ares("Ares","GODofWAR");
+                Ares ares = new Ares("Ares","GODofWAR");
                 break;
             case "Afina":
-                printable = new Afina("Afina","Женщина");
+                Afina afina = new Afina("Afina","Женщина");
                 break;
             case "Zevs":
-                printable = new Zevs("Zevs","Islam");
+                Zevs zevs = new Zevs("Zevs","Islam");
                 break;
         }
-        return printable;
+        return null;
     }
 }
